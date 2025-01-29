cdec347cd9e56b7704ab7b0ed56d28c79be52be7
Stefan Bearson <trancedude79@hotmail.com>
2025-01-29 19:29:17 +0100

    add blazor

---
+++ RegEx/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using System;
+
+namespace RegEx;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "Hello this is a test";
+        string regExPattern = @"^Hello";
+
+        Regex regex = new (regExPattern);
+
+        Match match = regex.Match(input);
+
+        if (match.Success)
+        {
+            Console.WriteLine("Success!");
+            Console.WriteLine($"{input} matches with ({regExPattern})");
+            Console.WriteLine($"Value: {match.Value}");
+        }
+
+        if (!match.Success)
+        {
+            Console.WriteLine("Sorry, no match!");
+        }
+
+        MatchCollection matches = regex.Matches(input);
+        foreach (Match m in matches)
+        {
+            Console.WriteLine("Email: " + m.Value);
+        }
+
+    }
+}
